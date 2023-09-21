9de9a2a606ec1d704b3f377dad0f05aac3105ea7
donnybrilliant <daniel.vier@gmail.com>
2023-09-21 10:19:37 +0200

    Changes repo structure

--- src/routes/repos.js
+++ src/routes/repos.js
@@ -1,17 +1,41 @@
 import { getRepositories } from "../services/api.js";
 
 function reposRoutes(app) {
-  app.get("/repos", async (req, res, next) => {
+  const fetchAndFormatRepos = async (repoType, req, res, next) => {
     try {
-      const type = req.query.type || "public";
-      const repos = await getRepositories(type);
-      const repoLinks = repos
-        .map((repo) => `<a href="${repo.html_url}">${repo.name}</a>`)
-        .join("<br>");
-      res.send(repoLinks);
+      const repos = await getRepositories(repoType);
+      const repoList = repos.map((repo) => {
+        const object = {
+          name: repo.name,
+          description: repo.description,
+          html_url: repo.html_url,
+          homepage: repo.homepage,
+          language: repo.language,
+          deployments: repo.deployments_url,
+        };
+
+        if (repo.has_pages) {
+          object.pages = `https://${repo.owner.login}.github.io/${repo.name}`;
+        } else {
+          object.pages = null;
+        }
+
+        return object;
+      });
+
+      return res.json(repoList);
     } catch (error) {
       next(error);
     }
+  };
+
+  app.get("/repos", (req, res, next) => {
+    // Default type if you want a different type for this route
+    fetchAndFormatRepos("public", req, res, next);
+  });
+
+  app.get("/repos/all", (req, res, next) => {
+    fetchAndFormatRepos("all", req, res, next);
   });
 }
 
